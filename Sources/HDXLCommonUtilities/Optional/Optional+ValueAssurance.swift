f69cb299ba611565cf140f6a3159ff548f5bb848
plx <plxgithub@gmail.com>
2019-07-03 09:23:32 -0500

    Value assurance.

---
+++ Sources/HDXLCommonUtilities/Optional/Optional+ValueAssurance.swift
@@ -0,0 +1,34 @@
+//
+//  Optional+ValueAssurance.swift
+//
+
+import Foundation
+
+public extension Optional {
+  
+  /// Utility function equivalent to a snippet like `optional ?? fallback`
+  /// except that if `fallback` is used we set `self = .some(fallback)` before
+  /// returning `fallback`.
+  ///
+  ///
+  /// - parameter assurance: A lazily-evaluated autoclosure used to provide the fallback value if needed.
+  ///
+  /// - returns: Either the pre-existing wrapped value or the fallback value provided-by "assurance".
+  ///
+  /// - postcondition: `self != nil`; the wrapped value will be the original value if there was one, otherwise it's the value obtained by evaluating `assurance`.
+  ///
+  /// - note: I'd consider an operator calling this *if* I had a good operator in mind, but nothing I can think of seems like a great choice right now.
+  ///
+  mutating func obtainAssuredValue(
+    fallingBackUpon assurance: @autoclosure () -> Wrapped) -> Wrapped {
+    switch self {
+    case .some(let value):
+      return value
+    case .none:
+      let value = assurance()
+      self = .some(value)
+      return value
+    }
+  }
+  
+}
